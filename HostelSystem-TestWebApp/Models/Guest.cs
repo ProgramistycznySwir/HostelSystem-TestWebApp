665dd966063420af23e703bbacfd5031d5c4e5e9
Programistyczny Swir <programistycznyswir@gmail.com>
2021-08-07 20:45:00 +0200

    init

---
+++ HostelSystem-TestWebApp/Models/Guest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HostelSystem_TestWebApp.Models
+{
+    public class Guest
+    {
+        //- Imię,
+        public string Name { get; set; }
+        //- Nazwisko,
+        public string Surname { get; set; }
+        //- E-mail,
+        public string Email { get; set; }
+        //- Id.
+        public int ID { get; set; }
+
+        //- Data urodzenia,
+        public DateTime BirthDate { get; set; }
+        //- Kod pocztowy
+        public string PostalCode { get; set; }
+    }
+}
---
+++ HostelSystem-TestWebApp/Models/Reservation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HostelSystem_TestWebApp.Models
+{
+    public class Reservation
+    {
+        //- Kod Rezerwacji(max 10 znaków),
+        public string Code { get; set; }
+        //- Daty utworzenia,
+        public DateTime CreationDate { get; set; }
+        //- Cena,
+        public int Price { get; set; }
+        //- Data zameldowania,
+        public DateTime CheckInDate { get; set; }
+        //- Data wymeldowania,
+        public DateTime CheckOutDate { get; set; }
+        //- Waluta,
+        public string CurrencyCode { get; set; }
+        //- Id.
+        // Nie ma potrzeby komplikować bazy przy pomocy long'a i BIGINT'a, więc tutaj wstawiam zwykłego int'a
+        public int ID { get; set; }
+
+        //- Prowizja,
+        public int Provision { get; set; }
+        //- Źródło.
+        public string Source { get; set; }
+    }
+}
