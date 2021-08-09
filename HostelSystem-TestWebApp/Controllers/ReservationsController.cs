ee1428552bc6fd60ac96b7dde7c3375b20bcce92
Programistyczny Swir <programistycznyswir@gmail.com>
2021-08-09 13:55:03 +0200

    Clean up

--- HostelSystem-TestWebApp/Controllers/ReservationsController.cs
+++ HostelSystem-TestWebApp/Controllers/ReservationsController.cs
@@ -22,70 +22,7 @@ namespace HostelSystem_TestWebApp.Controllers
             return Services.ReservationManager.GetAllReservations();
         }
 
-        // GET: api/Reservations/5
-        //[ResponseType(typeof(Reservation))]
-        //public IHttpActionResult GetReservation(int id)
-        //{
-        //    Reservation reservation = db.Reservations.Find(id);
-        //    if (reservation == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return Ok(reservation);
-        //}
-
-        // PUT: api/Reservations/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutReservation(int id, Reservation reservation)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != reservation.ID)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(reservation).State = EntityState.Modified;
-
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ReservationExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
         // POST: api/Reservations
-        [ResponseType(typeof(Reservation))]
-        //public IHttpActionResult PostReservation(string reservationString)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-        //    reservationString;
-
-
-        //    db.Reservations.Add(reservation);
-        //    db.SaveChanges();
-
-        //    return CreatedAtRoute("DefaultApi", new { id = reservation.ID }, reservation);
-        //}
         public IHttpActionResult PostReservation(Reservation reservation)
         {
             if (!ModelState.IsValid)
@@ -94,27 +31,10 @@ namespace HostelSystem_TestWebApp.Controllers
             }
             if(reservation == null)
                 return BadRequest(ModelState);
-
-            db.Reservations.Add(reservation);
-            db.SaveChanges();
+            
+            Services.ReservationManager.AddReservation(reservation);
 
             return CreatedAtRoute("DefaultApi", new { id = reservation.ID }, reservation);
-        }
-
-        // DELETE: api/Reservations/5
-        [ResponseType(typeof(Reservation))]
-        public IHttpActionResult DeleteReservation(int id)
-        {
-            Reservation reservation = db.Reservations.Find(id);
-            if (reservation == null)
-            {
-                return NotFound();
-            }
-
-            db.Reservations.Remove(reservation);
-            db.SaveChanges();
-
-            return Ok(reservation);
         }
 
         protected override void Dispose(bool disposing)
